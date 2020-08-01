72da191be73f070cd4d0c70df03fbfbf28078f2c
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-08-01 21:11:12 +0530

    logs

---
+++ logs/log-2020-08-01.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-08-01 11:51:29 --> 404 Page Not Found: Logviewer/index
+ERROR - 2020-08-01 11:51:53 --> 404 Page Not Found: Logviewer/index
