a81b5be6985bbbf78c77904f1ed9ed90ce474113
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-08-02 09:22:49 +0530

    T-Public, PHPError bugs

--- application/controllers/LogViewerController.php
+++ application/controllers/LogViewerController.php
@@ -1,9 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+class LogViewerController extends CI_Controller {
+
 private $logViewer;
 
 public function __construct() {
+
+       parent::__construct();
+        // load the library
+		$this->load->library('CILogViewer');
 		
     $this->logViewer = new CILogViewer();
 }
@@ -11,4 +17,5 @@ public function __construct() {
 public function index() {
     echo $this->logViewer->showLogs();
     return;
+}
 }
