68bee84e50b03c99a79f0c32650b9e4993c00755
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-08-04 20:28:28 +0530

    screenshot

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,8 +51,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['log/(\d{4})-(\d{2})-(\d{2})'] = 'log/$1-$2-$3';
 $route['default_controller'] = 'welcome/index';
-// $route['bslogs'] = "logViewerControllers/index";
-$route['hotlogs'] = "Hotlogs/index";
-$route['logme'] = "Logviewer9/index";
+$route['logviewer9'] = "Logviewer9/index";
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
