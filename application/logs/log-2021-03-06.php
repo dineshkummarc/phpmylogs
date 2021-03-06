bdbc8410d0492376846725065ced13e62bc5a8ff
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-03-07 01:18:26 +0530

    update

---
+++ application/logs/log-2021-03-06.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-03-06 19:30:07 --> Severity: Warning --> file_get_contents(logs_view.php): failed to open stream: No such file or directory C:\xampp\htdocs\Test\phpmylogs\application\libraries\CI_Telescope.php 110
